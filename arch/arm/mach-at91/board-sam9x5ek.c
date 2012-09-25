e73cdd2a49e2e7fe7d15cfc920c0c4d5785aca10
Sergio Tanzilli <tanzilli@acmesystems.it>
2012-09-25 17:26:54 +0200

    Definition of USB pins. Disabled eth1. Changed Machine name.

--- arch/arm/mach-at91/board-sam9x5ek.c
+++ arch/arm/mach-at91/board-sam9x5ek.c
@@ -88,10 +88,12 @@ static struct at91_eth_data __initdata ek_macb0_data = {
 	.is_rmii	= 1,
 };
 
+/*
 static struct at91_eth_data __initdata ek_macb1_data = {
 	.phy_irq_pin	= AT91_PIN_PC26,
 	.is_rmii	= 1,
 };
+*/
 
 
 /*
@@ -105,6 +107,7 @@ static struct mci_platform_data __initdata mci0_data = {
 	},
 };
 
+/*
 static struct mci_platform_data __initdata mci1_data = {
 	.slot[0] = {
 		.bus_width	= 4,
@@ -112,6 +115,7 @@ static struct mci_platform_data __initdata mci1_data = {
 		.wp_pin		= -1,
 	},
 };
+*/
 
 /*
  *  ISI
@@ -336,41 +340,15 @@ static struct i2c_board_info __initdata ek_i2c_devices[] = {
 
 static void __init ek_board_configure_pins(void)
 {
-	if (ek_is_revA()) {
-		/* Port A is shared with gadget port */
-		/*ek_usbh_fs_data.vbus_pin[0] = AT91_PIN_PD9;*/
-		/*ek_usbh_hs_data.vbus_pin[0] = AT91_PIN_PD9;*/
-		ek_usbh_fs_data.vbus_pin[1] = AT91_PIN_PD10;
-		ek_usbh_hs_data.vbus_pin[1] = AT91_PIN_PD10;
-		/* Port C is full-speed only */
-		ek_usbh_fs_data.vbus_pin[2] = AT91_PIN_PD11;
-
-		ek_usba_udc_data.vbus_pin = AT91_PIN_PB8;
-
-		ek_macb0_data.phy_irq_pin = 0;
-
-		mci0_data.slot[0].detect_pin = AT91_PIN_PD13;
-	} else {
-		/* Port A is shared with gadget port */
-		/*ek_usbh_fs_data.vbus_pin[0] = AT91_PIN_PD18;*/
-		/*ek_usbh_hs_data.vbus_pin[0] = AT91_PIN_PD18;*/
-		ek_usbh_fs_data.vbus_pin[1] = AT91_PIN_PD19;
-		ek_usbh_hs_data.vbus_pin[1] = AT91_PIN_PD19;
-		/* Port C is full-speed only */
-		ek_usbh_fs_data.vbus_pin[2] = AT91_PIN_PD20;
-
-		ek_usba_udc_data.vbus_pin = AT91_PIN_PB16;
-
-		ek_macb0_data.phy_irq_pin = AT91_PIN_PB8;
-
-		mci0_data.slot[0].detect_pin = AT91_PIN_PD15;
-
-#if defined(CONFIG_KEYBOARD_QT1070)
-		if (!cpu_is_at91sam9g25())
-			/* conflict with ISI */
-			at91_set_gpio_input(ek_i2c_devices[1].irq, 1);
-#endif
-	}
+	  ek_usbh_fs_data.vbus_pin[0] = AT91_PIN_PD9;
+	  ek_usbh_hs_data.vbus_pin[0] = AT91_PIN_PD9;
+	  ek_usbh_fs_data.vbus_pin[1] = AT91_PIN_PD10;
+	  ek_usbh_hs_data.vbus_pin[1] = AT91_PIN_PD10;
+	  Port C is full-speed only
+	  ek_usbh_fs_data.vbus_pin[2] = AT91_PIN_PD11;
+	  ek_usba_udc_data.vbus_pin = AT91_PIN_PB8;
+	
+	  ek_macb0_data.phy_irq_pin = 0;
 }
 
 static void __init ek_board_init(void)
@@ -388,64 +366,12 @@ static void __init ek_board_init(void)
 	at91_add_device_usba(&ek_usba_udc_data);
 	/* Ethernet */
 	at91_add_device_eth(0, &ek_macb0_data);
-	at91_add_device_eth(1, &ek_macb1_data);
+	/* at91_add_device_eth(1, &ek_macb1_data); */
 	/* MMC */
 	at91_add_device_mci(0, &mci0_data);
-	/* Conflict between SPI0 and MCI1 pins */
-	if (!(cm_config & CM_CONFIG_SPI0_ENABLE))
-		at91_add_device_mci(1, &mci1_data);
-	/* I2C */
-	if (cm_config & CM_CONFIG_I2C0_ENABLE)
-		i2c_register_board_info(0,
-				ek_i2c_devices, ARRAY_SIZE(ek_i2c_devices));
-	else
-		at91_add_device_i2c(0,
-				ek_i2c_devices, ARRAY_SIZE(ek_i2c_devices));
-
-	if (cpu_is_at91sam9g25()) {
-		/* ISI */
-		/* NOTE: PCK0 provides ISI_MCK to the ISI module.
-		   ISI's PWD pin conflict with MCI1_CK due the hardware design.
-		 */
-		platform_add_devices(soc_camera_devices,
-					ARRAY_SIZE(soc_camera_devices));
-		isi_set_clk();
-		at91_add_device_isi(&isi_data);
-	} else if (!cpu_is_at91sam9x25()) {
-		/* LCD Controller */
-		at91_add_device_lcdc(&ek_lcdc_data);
-		/* Touch Screen */
-		at91_add_device_tsadcc(&ek_tsadcc_data);
-	}
-
-#if 0
-	if (cpu_is_at91sam9x25() || cpu_is_at91sam9x35())
-		/*
-		 * open jumper/solderdrop JP11 to activate CAN0
-		 *
-		 * _note_: this will deactivate the debug uart
-		 */
-		at91_add_device_can(0, NULL);
-#endif
-
-	if (cpu_is_at91sam9x25() || cpu_is_at91sam9x35())
-		/* this conflicts with usart.1 */
-		at91_add_device_can(1, NULL);
-
-	/* Push Buttons */
-	if (ek_is_revA())
-		ek_add_device_buttons();
-
-	/* SSC (for WM8731) */
-	at91_add_device_ssc(AT91SAM9X5_ID_SSC, ATMEL_SSC_TX | ATMEL_SSC_RX);
-
-	if (ek_is_revA())
-		printk(KERN_CRIT "AT91: EK rev A\n");
-	else
-		printk(KERN_CRIT "AT91: EK rev B and higher\n");
 }
 
-MACHINE_START(AT91SAM9X5EK, "Atmel AT91SAM9X5-EK")
+MACHINE_START(AT91SAM9X5EK, "Acme Systems Aria G25")
 	/* Maintainer: Atmel */
 /* XXX/ukl: can we drop .boot_params? */
 	.boot_params	= AT91_SDRAM_BASE + 0x100,
